03c0e7f51af22855ed1ded4e1e4d221fd40bdfbe
CRSL <siva19xx@gmail.com>
2014-09-16 17:34:19 +0900

    instance page

--- Assets/InstantiatePage.cs
+++ Assets/InstantiatePage.cs
@@ -16,8 +16,13 @@ public class InstantiatePage : MonoBehaviour {
 	public string ImgPath = "ImageFolder";
 	private List<string> ImgList = new List<string>();
 	private List<Texture2D> TexList = new List<Texture2D> ();
-	private List<Sprite> SprList = new List<Sprite> ();
+	private List<Sprite> RSprList = new List<Sprite> ();
+	private List<Sprite> LSprList = new List<Sprite> ();
+	private List<GameObject> GObjList = new List<GameObject> ();
+
 	private int nImages; //画像の枚数
+	private int readingPage; //現在の位置(0~nImages)
+
 
 	//フォルダ内のファイル名前を取得・リストに格納、ファイル数の取得
 	void GetImgList (string path) {
@@ -63,8 +68,9 @@ public class InstantiatePage : MonoBehaviour {
 	public float frame_w = 1f;
 	public float frame_h = 1f;
 
-	//テクスチャとピボットをうけて収まるようにスプライトを返す。
-	Sprite setSprite(Texture2D tex,Vector2 pivot){
+	//テクスチャと左右どちらかか、をうけて収まるようにスプライトを返す。
+	//int pos 0,1,2 == R,L,C
+	Sprite setSprite(Texture2D tex,int pos){
 		int img_height = tex.height;
 		int img_width = tex.width;
 		float wr = (float)img_width/frame_w;
@@ -72,6 +78,16 @@ public class InstantiatePage : MonoBehaviour {
 		float pTU = wr; //pixel to unit
 		if(hr > wr){pTU = hr;} //長い辺がおさまるようにpTUを決める。
 		Rect rect = new Rect(0,0,img_width,img_height);
+		Vector2 pivot = new Vector2 (1f, 0.5f);
+		if (pos == 0) {
+			pivot.x = 0;
+		} else if (pos == 1) {
+			pivot.x = 1f;
+		} else if (pos == 2) {
+			pivot.x = 0.5f;
+		} else {
+			print ("error");
+		}
 		//pivot = new Vector2 (Mathf.Clamp01 (pivot.x), Mathf.Clamp01 (pivot.y));
 		sp = Sprite.Create(tex,rect,pivot,pTU);
 		return sp;
@@ -79,25 +95,49 @@ public class InstantiatePage : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		readingPage = 0;
 
 		GetImgList (ImgPath);
-
-		foreach (string str in ImgList) {
-			TexList.Add (ReadTexture (str)); //List<Texture2D> TexListにテクスチャを全部格納する。SpPivotを使うのでその設定をしてから。
+		if (nImages == 0) {
+			Application.Quit ();
 		}
 
-		Vector2 pagepivot = new Vector2 (0, 0.5f);
 
+		foreach (string str in ImgList) {
+			TexList.Add (ReadTexture (str)); //List<Texture2D> TexListにテクスチャを全部格納する。
+		}
+
+		for(int x=0;x < nImages; x++) {
+			RSprList.Add (setSprite (TexList[x],0));
+			LSprList.Add (setSprite (TexList[x],1));
+			print (ImgList[x]);
+		}
+		
 		for (int x = 0; x < nImages; x++) {
-			GameObject obj = Instantiate (page, new Vector3 ((float)x/nImages, 0, 0), Quaternion.identity) as GameObject;
-			obj.GetComponent<SpriteRenderer> ().sprite = setSprite (TexList[x],pagepivot);
+			GameObject obj = Instantiate (page, new Vector3 (-0.05f*(float)x/nImages, 0, 0), Quaternion.identity) as GameObject;
+			obj.GetComponent<SpriteRenderer> ().sprite = RSprList[x];
 			obj.name = "page"+x;
+			obj.transform.Rotate (0, 180 + 1 * (float)x / nImages ,0);
+			GObjList.Add (obj);
 		}
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetButtonDown ("next")) {
+			if(readingPage == nImages){
+				return;
+			}
+			GObjList[readingPage].transform.Rotate (0,-180,0);
+			readingPage++;
+		} else if (Input.GetButtonDown ("prev")) {
+			if(readingPage == 0){
+				return;
+			}
+			GObjList[readingPage-1].transform.Rotate (0,180,0);
+			readingPage--;
+
+		}
 	}
 }
